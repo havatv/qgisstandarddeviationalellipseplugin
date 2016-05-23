4d881a144aed591a2c0d1ef451af2c181f961154
havatv <havard.tveite@nmbu.no>
2016-05-23 10:05:23 +0200

    Added some more files

---
+++ i18n/nb.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>SDEllipse</name>
+    <message>
+        <location filename="SDEllipse.py" line="68"/>
+        <source>&amp;Standard deviational ellipse</source>
+        <translation>&amp;Standardavviksellipse</translation>
+    </message>
+</context>
+<context>
+    <name>SDEllipseDialog</name>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="62"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="63"/>
+        <source>Close</source>
+        <translation>Avslutt</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="64"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="110"/>
+        <source>No input layer defined</source>
+        <translation>Inndatasett mangler</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="113"/>
+        <source>No features in input layer</source>
+        <translation>Inndatasettet er tomt</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="117"/>
+        <source>Missing numerical field</source>
+        <translation>Ingen numeriske egenskaper</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="164"/>
+        <source>Aborted</source>
+        <translation>Avbrutt</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="181"/>
+        <source>Worker</source>
+        <translation>Arbeider</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="189"/>
+        <source>Killing worker</source>
+        <translation>Dreper arbeidsprosessen</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="295"/>
+        <source>Error</source>
+        <translation>Feil</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="304"/>
+        <source>Warning</source>
+        <translation>Advarsel</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="313"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="61"/>
+        <source>SD Ellipse</source>
+        <translation>SD Ellipse</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_dialog.py" line="166"/>
+        <source>Not able to create ellipse</source>
+        <translation>Klarte ikke å lage ellipse</translation>
+    </message>
+</context>
+<context>
+    <name>Worker</name>
+    <message>
+        <location filename="SDEllipse_engine.py" line="66"/>
+        <source>No input layer defined</source>
+        <translation>Inndatasett mangler</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_engine.py" line="109"/>
+        <source>Weights add to zero</source>
+        <translation>Summen av vektene er 0</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse_engine.py" line="149"/>
+        <source>Weights add to zero or identical points</source>
+        <translation>Summen av vektene er 0, eller punktene er like</translation>
+    </message>
+</context>
+<context>
+    <name>linedirectionhistogramDialogBase</name>
+    <message>
+        <location filename="SDEllipse.ui" line="59"/>
+        <source>Selected features only</source>
+        <translation>Kun valgte objekter</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse.ui" line="14"/>
+        <source>Standard deviational ellipse</source>
+        <translation>Standardavviksellipse</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse.ui" line="32"/>
+        <source>Input (point) vector layer</source>
+        <translation>Inndatasett (vektor, punkt)</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse.ui" line="66"/>
+        <source>Use weights</source>
+        <translation>Bruk vekting</translation>
+    </message>
+    <message>
+        <location filename="SDEllipse.ui" line="73"/>
+        <source>Weight field</source>
+        <translation>Vektingsfelt</translation>
+    </message>
+</context>
+</TS>
