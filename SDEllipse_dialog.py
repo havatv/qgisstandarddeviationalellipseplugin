128bd8a78524c20c1e83b7f752075243173d8bb2
havatv <havard.tveite@nmbu.no>
2018-08-17 09:42:54 +0200

    Closing the SDE polygon (issue #6)

--- SDEllipse_dialog.py
+++ SDEllipse_dialog.py
@@ -315,6 +315,10 @@ class SDEllipseDialog(QDialog, FORM_CLASS):
                          minorSD * sin(t) * cos(majoraxisangle))
             points.append(QgsPoint(p1))
             t = t + step
+        # Close the polygon
+        p1 = QPointF(meanx + majorSD * cos(majoraxisangle),
+                         meany + majorSD * sin(majoraxisangle))
+        points.append(QgsPoint(p1))
         sdfeature.setGeometry(QgsGeometry.fromPolygon([points]))
         attrs = [meanx, meany, majoraxisangle, direction,
                  majorSD, minorSD, eccentricity]
