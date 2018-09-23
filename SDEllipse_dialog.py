3b0b59087dbf8cf030d90ccbbdeabba8b293365c
H Tveite <havard.tveite@nmbu.no>
2018-09-23 23:00:34 +0200

    Fixed issue with integer fields from Shapefile format datasets not being accepted as weights (#9)

--- SDEllipse_dialog.py
+++ SDEllipse_dialog.py
@@ -363,8 +363,9 @@ class SDEllipseDialog(QDialog, FORM_CLASS):
                 attrdict = atr
             for id, attrib in attrdict.items():
                 # Check for numeric attribute
-                if attrib.typeName().upper() in ('REAL', 'INTEGER', 'INT4',
-                                                 'INT8', 'FLOAT4'):
+                #if attrib.typeName().upper() in ('REAL', 'INTEGER', 'INT4',
+                #                                 'INT8', 'FLOAT4'):
+                if attrib.isNumeric():
                     self.inputField.addItem(attrib.name(), attrib.name())
             if (self.inputField.count() > 0):
                 if self.method == 1:
